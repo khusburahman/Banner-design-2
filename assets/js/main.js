ee5d7d4c6a4d27a722c703add6740f50d6840c1a
khusburahman <khusburahman49@gmail.com>
2024-02-21 21:30:51 +0600

    dashboard design-2 done

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// const toggle=document.getElementById('toggle');
+// const menu=document.querySelector('.menu');
+
+// toggle.onclick=function(){
+//     menu.classList.toggle('active')
+// }
+// gsap.from('img',1.2,{opacity:0,x:300,delay:0.3})
+// gsap.from('h3',1.2,{opacity:0,y:-100,delay:0.6})
+// gsap.from('button',1.2,{opacity:0,y:100,delay:0.9})
+// gsap.from('h2',1.2,{opacity:0,x:-300,delay:0.7})
+// gsap.from('p',1.2,{opacity:0,x:-300,delay:0.8})
+// gsap.from('.social-box',1.2,{opacity:0,y:-150,delay:1})
+// gsap.from('header',1.2,{opacity:0,y:-80,delay:0.5})
+// gsap.from('overly',1.2,{opacity:0,y:0,delay:0.3})
